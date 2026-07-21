37a3bb67d70b36775091f062fd020ac043e2004b
Eren <eren@Erens-MacBook-Pro.local>
2026-07-21 13:31:49 +0300

    feat: enable all 7 console systems with working archive.org collections

--- src/emu_table.c
+++ src/emu_table.c
@@ -6,13 +6,13 @@
  * with no files[], not a scraping bug. Re-check occasionally; archive.org
  * can un-dark a collection just as easily as it darked it. */
 const EmuEntry EMU_TABLE[] = {
-    {"PS",  "PlayStation",           "chd_psx_eur",   "chd", 1, "Sony_-_PlayStation"},
-    {"MD",  "Sega Genesis",          "nointro.md",    "7z",  1, "Sega_-_Mega_Drive_-_Genesis"},
-    {"GB",  "Game Boy",              "nointro.gb",    "7z",  0, NULL},
-    {"GBC", "Game Boy Color",        "nointro.gbc-1", "7z",  0, NULL},
-    {"GBA", "Game Boy Advance",      "nointro.gba",   "7z",  0, NULL},
-    {"FC",  "NES",                   "nointro.nes",   "7z",  0, NULL},
-    {"SFC", "SNES",                  "nointro.snes",  "7z",  0, NULL},
+    {"PS",  "PlayStation",           "chd_psx_eur",                                         "chd", 1, "Sony_-_PlayStation"},
+    {"MD",  "Sega Genesis",          "nointro.md",                                          "7z",  1, "Sega_-_Mega_Drive_-_Genesis"},
+    {"GB",  "Game Boy",              "game-boy-and-game-boy-color-complete-collection",     "gb",  1, "Nintendo_-_Game_Boy"},
+    {"GBC", "Game Boy Color",        "game-boy-and-game-boy-color-complete-collection",     "gbc", 1, "Nintendo_-_Game_Boy_Color"},
+    {"GBA", "Game Boy Advance",      "CentralArquivista-GameBoyAdvance",                    "gba", 1, "Nintendo_-_Game_Boy_Advance"},
+    {"FC",  "NES",                   "nintendo-entertainment-system-nes-roms-europeusa",  "nes", 1, "Nintendo_-_Nintendo_Entertainment_System"},
+    {"SFC", "SNES",                  "CentralArquivista-SuperNintendo",                     "sfc", 1, "Nintendo_-_Super_Nintendo_Entertainment_System"},
 };
 
 const int EMU_TABLE_COUNT = sizeof(EMU_TABLE) / sizeof(EMU_TABLE[0]);
